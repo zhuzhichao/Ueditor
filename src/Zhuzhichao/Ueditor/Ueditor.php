dd1a4ef45685dabf2ccd6519ba9710540b523f53
zhuzhichao <me@zhuzhichao.com>
2015-01-08 17:29:45 +0800

    添加注释

--- src/Zhuzhichao/Ueditor/Ueditor.php
+++ src/Zhuzhichao/Ueditor/Ueditor.php
@@ -1,12 +1,30 @@
 <?php namespace Zhuzhichao\Ueditor;
 
+/**
+ * 提供uedior前端使用的方法
+ *
+ * Class Ueditor
+ * @package Zhuzhichao\Ueditor
+ */
 class Ueditor {
+
+	/**
+	 * 编辑器DOM
+	 * @param string $content
+	 * @param array  $config
+	 */
 	public static function content($content = '', $config = []) {
 
 		$attr = Ueditor::makeConfig2String($config);
 		echo "<script type='text/plain' {$attr}>{$content}</script>";
 	}
 
+	/**
+	 * 生成编辑器的参数
+	 * @param $config
+	 *
+	 * @return string
+	 */
 	private static function makeConfig2String($config) {
 		$string = '';
 		if (is_array($config)) {
@@ -23,10 +41,16 @@ class Ueditor {
 		return $string;
 	}
 
+	/**
+	 *	编辑器的CSS资源
+	 */
 	public static function css() {
 		echo '<link href="'.asset('packages/zhuzhichao/ueditor/themes/default/css/ueditor.css').'" type="text/css" rel="stylesheet">';
 	}
 
+	/**
+	 *	编辑器的JS资源
+	 */
 	public static function js() {
 		echo '<script type="text/javascript" charset="utf-8" src="'.route('ueditor.config').'"></script>';
 		echo '<script type="text/javascript" charset="utf-8" src="'.asset('packages/zhuzhichao/ueditor/ueditor.all.min.js').'"></script>
